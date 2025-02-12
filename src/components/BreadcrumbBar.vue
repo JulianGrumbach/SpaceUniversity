3c4790ee57db7e635d7ab5f9e004684067fdce26
Julian Grumbach <s4jugrum@uni-trier.de>
2025-02-12 23:06:02 +0100

    Breadcrumbs fix

--- src/components/BreadcrumbBar.vue
+++ src/components/BreadcrumbBar.vue
@@ -5,14 +5,30 @@
   />
 </template>
 <script setup lang="ts">
-  import { ref } from 'vue';
+  import { reactive } from 'vue';
 
   //Remove later
-  const state = ref({
+  const state = reactive({
     items: [
-      { title: 'Home', disabled: false, href: '/' },
-      { title: 'About', disabled: false, href: '/about' },
-      { title: 'Contact', disabled: true },
+      { title: 'Home', disabled: false, href: '/' }
     ],
   })
+
+  //Watch route changes
+  const route = useRoute();
+  route.path.split('/').forEach((item, index) => {
+      if (index === 0) return;
+      const newTitle = item.charAt(0).toUpperCase() + item.slice(1);
+      state.items[index + 1] = { title: newTitle, disabled: false, href: `${item}` };
+    });
+  watch(() => route.path, () => {
+    console.log('Route changed:', route.path);
+    //Clear all items except the first one
+    state.items.splice(1);
+    route.path.split('/').forEach((item, index) => {
+      if (index === 0) return;
+      const newTitle = item.charAt(0).toUpperCase() + item.slice(1);
+      state.items[index + 1] = { title: newTitle, disabled: false, href: `${item}` };
+    });
+  });
 </script>
